edd0b236c8add1dfd96b28c71c3c0d17ce5f522c
Elias Getachew <elilidu1990@gmail.com>
2024-09-19 21:52:59 -0400

    add comment

--- program.cs
+++ program.cs
@@ -7,7 +7,13 @@ namespace HelloWorld
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
-            Console.WriteLine("next line");
-    }
+            //Console.WriteLine("Welcome to Acme Accounting Systems \n Remember, we’re “accounting");
+            Console.WriteLine("What is your name?");// 
+            String yourName = Console.ReadLine();  // Reading the name and assign the Text value on yourname identifyer
+            Console.WriteLine("What course are you on ?");
+            String course = Console.ReadLine();    // Reading the coures  and assign the Text value on course identifyer
+            Console.WriteLine("What page Number ?");
+            String pagen = Console.ReadLine();    // 
+        }
   }
 }

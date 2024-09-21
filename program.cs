bd33bd3c421b20297c5efd3aa800f80bdf256d85
Elias Getachew <elilidu1990@gmail.com>
2024-09-20 21:54:53 -0400

    add DUI

--- program.cs
+++ program.cs
@@ -6,83 +6,92 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
-            //Console.WriteLine("Welcome to Acme Accounting Systems \n Remember, we’re “accounting");
-            Console.WriteLine("What is your name?");// 
-            String yourName = Console.ReadLine();  // Reading the name and assign the Text value on yourname identifyer
-            Console.WriteLine("What course are you on ?");
-            String course = Console.ReadLine();    // Reading the coures  and assign the Text value on course identifyer
-            Console.WriteLine("What page Number ?");
-            String pagen = Console.ReadLine();    // 
-            String pagenumber = pagen.ToString(); // convert page number into String and set on pagenumber varible.
-            Console.WriteLine("Do you need any help with anything ? Pleas answer \" true \" or \" false\" );
-            String ans = Console.ReadLine(); // Reading the answer and assign the Text value on ans identifyer
-            bool respond = bool.Parse(ans);
-            Console.WriteLine("Is threre any other feedback you'd like to provide? Pleas be specific.");
-            String feedback = Console.ReadLine(); // Reading the feed back and assign the Text value on feedback identifyer
-            Console.WriteLine("How many hour did you study today ?");
-            int studyhour = Console.ReadLine(); // Reading study hour and assign the Text value on studyhour identifyer
-            Console.WriteLine("Thank you for your answers !");
-            // Adding two number and display the result
-            int num1 = 5;
-            int num2 = 7;
-            int total = num1 + num2;
-            Console.WriteLine("The sum is  " + total);
+            /* Console.WriteLine("Hello World!");
+         //Console.WriteLine("Welcome to Acme Accounting Systems \n Remember, we’re “accounting");
+         Console.WriteLine("What is your name?");// 
+         String yourName = Console.ReadLine();  // Reading the name and assign the Text value on yourname identifyer
+         Console.WriteLine("What course are you on ?");
+         String course = Console.ReadLine();    // Reading the coures  and assign the Text value on course identifyer
+         Console.WriteLine("What page Number ?");
+         String pagen = Console.ReadLine();    // 
+         String pagenumber = pagen.ToString(); // convert page number into String and set on pagenumber varible.
+         Console.WriteLine("Do you need any help with anything ? Pleas answer \" true \" or \" false\" );
+         String ans = Console.ReadLine(); // Reading the answer and assign the Text value on ans identifyer
+         bool respond = bool.Parse(ans);
+         Console.WriteLine("Is threre any other feedback you'd like to provide? Pleas be specific.");
+         String feedback = Console.ReadLine(); // Reading the feed back and assign the Text value on feedback identifyer
+         Console.WriteLine("How many hour did you study today ?");
+         int studyhour = Console.ReadLine(); // Reading study hour and assign the Text value on studyhour identifyer
+         Console.WriteLine("Thank you for your answers !");
+         // Adding two number and display the result
+         int num1 = 5;
+         int num2 = 7;
+         int total = num1 + num2;
+         Console.WriteLine("The sum is  " + total);
 
-            // Subtract two number and display the result
-            int num3 = 5;
-            int num4 = 7;
-            int diff = num1 - num2;
-            Console.WriteLine("The differnce is  " + diff);
+         // Subtract two number and display the result
+         int num3 = 5;
+         int num4 = 7;
+         int diff = num1 - num2;
+         Console.WriteLine("The differnce is  " + diff);
 
-            // Multiple two number and display the result
-            int num5 = 5;
-            int num6 = 7;
-            int mul = num1 + num2;
-            Console.WriteLine("The productis  " + mul);
+         // Multiple two number and display the result
+         int num5 = 5;
+         int num6 = 7;
+         int mul = num1 + num2;
+         Console.WriteLine("The productis  " + mul);
 
-            // Divided a number and display the result
-            double num7 = 5.0;
-            double num8 = 7.0;
-            double div = num2 / num1;
-            Console.WriteLine("Division is  " + div);
+         // Divided a number and display the result
+         double num7 = 5.0;
+         double num8 = 7.0;
+         double div = num2 / num1;
+         Console.WriteLine("Division is  " + div);
+         Console.ReadLine();
+
+         //Takes an input from the user, multiplies it by 50 and display the result
+         Console.WriteLine("enter the number larger than 10,000,000");
+         string userinput = Console.ReadLine();
+         long mul1 = long.Parse(userinput) * 50;
+         Console.WriteLine("The resulte is" + mul1);
+         Console.ReadLine();
+
+         //Takes an input from the user, adds 25 and display the result
+         Console.WriteLine("enter any Integetr number");
+         string usernum = Console.ReadLine();
+         long mul2 = long.Parse(usernum) * 25;
+         Console.WriteLine("The resulte is" + mul2);
+         Console.ReadLine();
+
+         //Takes an input from the user, divides it by 12.5 and display the resulte
+         Console.WriteLine("enter any number");
+         string usernum2 = Console.ReadLine();
+         Double div1 = Double.Parse(usernum2) / 12.5;
+         Console.WriteLine("The resulte is" + div1);
+         Console.ReadLine();
+
+         //Takes an input from the user, checks if it is greater than 50 and diplay the resulte
+         Console.WriteLine("enter any number");
+         string usernum3 = Console.ReadLine();
+         bool isLargeNum = int.Parse(usernum3) > 50;
+         Console.WriteLine(isLargeNum.ToString());
+         Console.ReadLine();
+
+         //Takes an input from the user, divides it by 7 and display the remainder
+         Console.WriteLine("enter any Integetr number");
+         string usernum4 = Console.ReadLine();
+         long num = long.Parse(usernum4) % 7;
+         Console.WriteLine("The remender is  " + num);*/
+
+            Console.WriteLine("What is your age?");
+            int age= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI Answer yes or no?");
+            int ans Console.ReadLine();
+            string nuOfTibker=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualifide for insuranves?");
+            bool qual = (age>15) && !ans)&& (nuOfTibker<=3);
+            Console.WriteLine(qual);
             Console.ReadLine();
 
-            //Takes an input from the user, multiplies it by 50 and display the result
-            Console.WriteLine("enter the number larger than 10,000,000");
-            string userinput = Console.ReadLine();
-            long mul1 = long.Parse(userinput) * 50;
-            Console.WriteLine("The resulte is" + mul1);
-            Console.ReadLine();
-
-            //Takes an input from the user, adds 25 and display the result
-            Console.WriteLine("enter any Integetr number");
-            string usernum = Console.ReadLine();
-            long mul2 = long.Parse(usernum) * 25;
-            Console.WriteLine("The resulte is" + mul2);
-            Console.ReadLine();
-
-            //Takes an input from the user, divides it by 12.5 and display the resulte
-            Console.WriteLine("enter any number");
-            string usernum2 = Console.ReadLine();
-            Double div1 = Double.Parse(usernum2) / 12.5;
-            Console.WriteLine("The resulte is" + div1);
-            Console.ReadLine();
-
-            //Takes an input from the user, checks if it is greater than 50 and diplay the resulte
-            Console.WriteLine("enter any number");
-            string usernum3 = Console.ReadLine();
-            bool isLargeNum = int.Parse(usernum3) > 50;
-            Console.WriteLine(isLargeNum.ToString());
-            Console.ReadLine(); */
-
-            //Takes an input from the user, divides it by 7 and display the remainder
-            Console.WriteLine("enter any Integetr number");
-            string usernum4 = Console.ReadLine();
-            long num = long.Parse(usernum4) % 7;
-            Console.WriteLine("The remender is  " + num);
-
-            Console.ReadLine();
         }
   }
 }
